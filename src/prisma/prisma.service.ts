761005ad6871405b20f7e83dbe992ac3ac92d537
Pedro Silva <pedro_rike@hotmail.com.br>
2026-01-27 23:43:38 -0300

    feat: create create account route, config client http and zod validation

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -1,13 +1,20 @@
-import { Injectable } from "@nestjs/common";
-import { PrismaPg } from "@prisma/adapter-pg";
+import { Injectable, OnModuleDestroy, OnModuleInit } from "@nestjs/common";
 import { PrismaClient } from "@prisma/client";
 
 @Injectable()
-export class PrismaService extends PrismaClient {
+export class PrismaService
+  extends PrismaClient
+  implements OnModuleInit, OnModuleDestroy
+{
   constructor() {
-    const adapter = new PrismaPg({
-      connectionString: process.env.DATABASE_URL as string,
-    });
-    super({ adapter });
+    super({ log: ["warn", "error"] });
+  }
+
+  async onModuleInit() {
+    return this.$connect();
+  }
+
+  async onModuleDestroy() {
+    return this.$disconnect();
   }
 }
