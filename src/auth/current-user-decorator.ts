c56d29cc0c6c818368b6b680da0751405cef9e3a
Pedro Silva <pedro_rike@hotmail.com.br>
2026-03-03 23:12:34 -0300

    feat: controller to create question

---
+++ src/auth/current-user-decorator.ts
@@ -0,0 +1,9 @@
+import { createParamDecorator, ExecutionContext } from "@nestjs/common";
+import { UserPayload } from "./jwt.strategy";
+
+export const CurrentUser = createParamDecorator(
+  (_: unknown, context: ExecutionContext) => {
+    const request = context.switchToHttp().getRequest();
+    return request.user as UserPayload;
+  },
+);
--- src/controllers/create-question.controller.ts
+++ src/controllers/create-question.controller.ts
@@ -1,7 +1,19 @@
-import { Controller, Post, UseGuards, UsePipes } from "@nestjs/common";
+import { Body, Controller, Post, UseGuards, UsePipes } from "@nestjs/common";
 import { JwtService } from "@nestjs/jwt";
+import { CurrentUser } from "src/auth/current-user-decorator";
 import { JwtAuthGuard } from "src/auth/jwt-auth.guard";
+import { UserPayload } from "src/auth/jwt.strategy";
+import { ZodValidationPipe } from "src/pipes/zod-validation-pipe";
 import { PrismaService } from "src/prisma/prisma.service";
+import z from "zod";
+
+const createQuestionBodySchema = z.object({
+  title: z.string(),
+  content: z.string(),
+});
+
+const bodyValidationPipe = new ZodValidationPipe(createQuestionBodySchema);
+type CreateQuestionBodySchema = z.infer<typeof createQuestionBodySchema>;
 
 @Controller("/questions")
 @UseGuards(JwtAuthGuard)
@@ -13,7 +25,30 @@ export class CreateQuestionController {
 
   @Post()
   @UsePipes()
-  async handle() {
-    return {};
+  async handle(
+    @Body(bodyValidationPipe) body: CreateQuestionBodySchema,
+    @CurrentUser() user: UserPayload,
+  ) {
+    const userId = user.sub;
+    const { title, content } = body;
+    const slug = this.convertToSlug(title);
+
+    await this.prisma.questions.create({
+      data: {
+        title,
+        content,
+        slug,
+        authorId: userId,
+      },
+    });
+  }
+
+  private convertToSlug(title: string): string {
+    return title
+      .toLowerCase()
+      .normalize("NFD")
+      .replace(/[\u0300-\u036f]/g, "")
+      .replace(/[^\w\s-]/g, "")
+      .replace(/\s+/g, "-");
   }
 }
