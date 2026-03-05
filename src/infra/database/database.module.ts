fddf057ea20a8a1b28b29ad04bf1153f884ebe05
Pedro Silva <pedro_rike@hotmail.com.br>
2026-03-04 23:34:51 -0300

    feat: implement repositories layers

---
+++ src/infra/database/database.module.ts
@@ -0,0 +1,30 @@
+import { Module } from "@nestjs/common";
+import { PrismaService } from "@/infra/database/prisma/prisma.service";
+import { PrismaQuestionRepository } from "./prisma/repositories/prisma-question-repository";
+import { PrismaAnswerAttachmentsRepository } from "./prisma/repositories/prisma-answer-attachments-repository";
+import { PrismaAnswerRepository } from "./prisma/repositories/prisma-answer-repository";
+import { PrismaAnswerCommentsRepository } from "./prisma/repositories/prisma-answer-comments-repository";
+import { PrismaQuestionCommentsRepository } from "./prisma/repositories/prisma-question-comments-repository";
+import { PrismaQuestionAttachmentsRepository } from "./prisma/repositories/prisma-question-attachments-repository";
+
+@Module({
+  providers: [
+    PrismaService,
+    PrismaQuestionRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaAnswerRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswerAttachmentsRepository,
+  ],
+  exports: [
+    PrismaService,
+    PrismaQuestionRepository,
+    PrismaQuestionCommentsRepository,
+    PrismaQuestionAttachmentsRepository,
+    PrismaAnswerRepository,
+    PrismaAnswerCommentsRepository,
+    PrismaAnswerAttachmentsRepository,
+  ],
+})
+export class DatabaseModule {}
