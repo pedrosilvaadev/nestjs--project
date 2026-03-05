83e4ec5ef4ee87fd465f84ba781582d41fe1c537
Pedro Silva <pedro_rike@hotmail.com.br>
2026-03-04 23:55:53 -0300

    feat: add connection between layers

--- src/infra/database/prisma/repositories/prisma-question-repository.ts
+++ src/infra/database/prisma/repositories/prisma-question-repository.ts
@@ -2,11 +2,21 @@ import { PaginationParams } from "@/core/repositories/pagination-params";
 import { QuestionsRepository } from "@/domain/forum/application/repositories/question-repository";
 import { Question } from "@/domain/forum/enterprise/entities/question";
 import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../prisma.service";
+import { PrismaQuestionMapper } from "../mappers/prisma-question-mapper";
 
 @Injectable()
 export class PrismaQuestionRepository implements QuestionsRepository {
-  findById(id: string): Promise<Question | null> {
-    throw new Error("Method not implemented.");
+  constructor(private prisma: PrismaService) {}
+  async findById(id: string): Promise<Question | null> {
+    const question = await this.prisma.questions.findUnique({
+      where: { id },
+    });
+
+    if (!question) {
+      return null;
+    }
+    return PrismaQuestionMapper.toDomain(question);
   }
   findBySlug(slug: string): Promise<Question | null> {
     throw new Error("Method not implemented.");
